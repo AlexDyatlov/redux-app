ae73e2808f3b6c7779912c157d57666d12206ca3
AlexDyatlov <aleks78.gooogle@gmail.com>
2022-11-11 14:13:09 +0300

    Redux implemented

---
+++ src/store/taskReducer.js
@@ -0,0 +1,26 @@
+import { taskUpdated } from "./actionTypes";
+import { taskDeleted } from "./actionTypes";
+
+export function taskReducer(state = [], action) {
+  switch (action.type) {
+    case taskUpdated: {
+      const newArray = [...state];
+      const elementIndex = newArray.findIndex(
+        (el) => el.id === action.payload.id
+      );
+      newArray[elementIndex] = {
+        ...newArray[elementIndex],
+        ...action.payload,
+      };
+      return newArray;
+    }
+    case taskDeleted: {
+      const newArray = [...state];
+      const arr = newArray.filter((el) => el.id !== action.payload.id);
+
+      return arr;
+    }
+    default:
+      return state;
+  }
+}
